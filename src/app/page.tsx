aeefd3ce0ffb3734f268971ec1ba7d0fd8b35a40
salamer <salamer_gaga@163.com>
2023-10-30 22:51:07 +0800

    update: update app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import { Leapcell } from "@leapcell/leapcell-js";
 
 async function getData() {
   const api = new Leapcell({
-    apiKey: process.env.LEAPCELL_API_KEY!,
+    apiKey: process.env.LP_API_KEY!,
   });
-  const table = api
-    .repo("salamer/myblog")
-    .table("tbl1702369503563026432", "name");
-  const res = await table.records.findMany({});
-  return res;
+  const resource = process.env.RESOURCE || "salamer/econ";
+  const tableId = process.env.TABLE_ID || "tbl1718517096606511104";
+  const table = api.repo(resource).table(tableId, "name");
+  const res = await table.records.findMany();
+  return {
+    products: res,
+    category: undefined,
+  };
 }
 
 export default async function Home() {
-  const data = await getData();
+  const { products } = await getData();
 
   return (
     <body>
@@ -23,27 +26,47 @@ export default async function Home() {
       </Head>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="sr-only">Products</h2>
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 text-center mb-16">
+            Leapcell Surfboards Store
+          </h1>
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {data.map((item) => {
-              const covers = item.fields["Cover"] as string[] || [];
+            {products.map((item) => {
+              const covers = (item.fields["Cover"] as string[]) || [];
               const cover = covers[0] || "";
+              const categories = (item.fields["Categories"] as string[]) || [];
+              const status = item.fields["Status"];
               return (
-                <a href={`/product/${item.record_id}`} className="group">
-                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                    <img
-                      src={cover}
-                      alt="Front of men&#039;s Basic Tee in black."
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
+                <div className="group">
+                  <a href={`/product/${item.record_id}`}>
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                      <img
+                        src={cover}
+                        alt="Front of men&#039;s Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <h3 className="mt-4 text-sm text-gray-700">
+                      {item.fields["Name"]}
+                      {status && (
+                        <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800">
+                          {status}
+                        </span>
+                      )}
+                    </h3>
+                    <p className="mt-1 text-lg font-medium text-gray-900">
+                      ${item.fields["Price"]}
+                    </p>
+                  </a>
+                  <div>
+                    {categories.map((category) => {
+                      return (
+                        <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800">
+                          {category}
+                        </span>
+                      );
+                    })}
                   </div>
-                  <h3 className="mt-4 text-sm text-gray-700">
-                    {item.fields["name"]}
-                  </h3>
-                  <p className="mt-1 text-lg font-medium text-gray-900">
-                    ${item.fields["Price"]}
-                  </p>
-                </a>
+                </div>
               );
             })}
           </div>
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -4,11 +4,11 @@ import { Leapcell, Record } from "@leapcell/leapcell-js";
 
 async function getData(id: string) {
   const api = new Leapcell({
-    apiKey: process.env.LEAPCELL_API_KEY!,
+    apiKey: process.env.LP_API_KEY!,
   });
-  const table = api
-    .repo("salamer/myblog")
-    .table("tbl1702369503563026432", "name");
+  const resource = process.env.RESOURCE || "salamer/econ";
+  const tableId = process.env.TABLE_ID || "tbl1718517096606511104";
+  const table = api.repo(resource).table(tableId, "name");
   const res = await table.records.findById(id);
   return res;
 }
@@ -19,118 +19,140 @@ export default async function Page({
   params: { id: string };
 }) {
   const data = await getData(id);
-  const skus = data.fields["SKU"] as string[] || [];
+  const skus = (data.fields["Styles"] as string[]) || [];
+  const sizes = (data.fields["Sizes"] as string[]) || [];
   return (
     <div>
       <Head>
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
 
-      <div className="bg-white">        
+      <div className="bg-white">
         <div className="pt-6">
-        <nav aria-label="Breadcrumb">
-                <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <li>
-                        <div className="flex items-center">
-                            <a href="/" className="mr-2 text-sm font-medium text-gray-900">Surfboard</a>
-                            <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true"
-                                className="h-5 w-4 text-gray-300">
-                                <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-                            </svg>
-                        </div>
-                    </li>
-                </ol>
-            </nav>
+          <nav aria-label="Breadcrumb">
+            <ol
+              role="list"
+              className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
+            >
+              <li>
+                <div className="flex items-center">
+                  <a
+                    href="/"
+                    className="mr-2 text-sm font-medium text-gray-900"
+                  >
+                    Surfboard
+                  </a>
+                  <svg
+                    width="16"
+                    height="20"
+                    viewBox="0 0 16 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    className="h-5 w-4 text-gray-300"
+                  >
+                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                  </svg>
+                </div>
+              </li>
+            </ol>
+          </nav>
 
-            <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-                {skus.map((item) => {
-                    return (
-                        <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-                            <img src={item} alt="Two each of gray, white, and black shirts laying flat."
-                                className="h-full w-full object-cover object-center" />
-                        </div>
-                    );
-                })}
+          <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+            {skus.map((item) => {
+              return (
+                <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+                  <img
+                    src={item}
+                    alt="Two each of gray, white, and black shirts laying flat."
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+            <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+              <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+                {data.fields["Name"]}
+              </h1>
             </div>
 
-            <div
-                className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-                <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                    <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{ data.fields["name"] }
-                    </h1>
-                </div>
+            <div className="mt-4 lg:row-span-3 lg:mt-0">
+              <h2 className="sr-only">Product information</h2>
+              <p className="text-3xl tracking-tight text-gray-900">
+                ${data.fields["Price"]}
+              </p>
 
-                <div className="mt-4 lg:row-span-3 lg:mt-0">
-                    <h2 className="sr-only">Product information</h2>
-                    <p className="text-3xl tracking-tight text-gray-900">${ data.fields["Price"] }</p>
+              <form className="mt-10">
+                <div className="mt-10">
+                  <div className="flex items-center justify-between">
+                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
+                    <a
+                      href="#"
+                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Size guide
+                    </a>
+                  </div>
 
-
-
-                    <form className="mt-10">
-
-                        <div className="mt-10">
-                            <div className="flex items-center justify-between">
-                                <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                                <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">Size
-                                    guide</a>
-                            </div>
-
-                            <fieldset className="mt-4">
-                                <legend className="sr-only">Choose a size</legend>
-                                <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                                    <label
-                                        className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-not-allowed bg-gray-50 text-gray-200">
-                                        <input type="radio" name="size-choice" value="XXS" disabled className="sr-only"
-                                            aria-labelledby="size-choice-0-label" />
-                                        <span id="size-choice-0-label">XXS</span>
-                                        <span aria-hidden="true"
-                                            className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200">
-                                            <svg className="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                                viewBox="0 0 100 100" preserveAspectRatio="none" stroke="currentColor">
-                                                <line x1="0" y1="100" x2="100" y2="0"
-                                                    vector-effect="non-scaling-stroke" />
-                                            </svg>
-                                        </span>
-                                    </label>
-                                    <label
-                                        className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
-                                        <input type="radio" name="size-choice" value="XS" className="sr-only"
-                                            aria-labelledby="size-choice-1-label"/ >
-                                        <span id="size-choice-1-label">XS</span>
-   
-                                        <span className="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <label
-                                        className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
-                                        <input type="radio" name="size-choice" value="S" className="sr-only"
-                                            aria-labelledby="size-choice-2-label" />
-                                        <span id="size-choice-2-label">S</span>
-                                        <span className="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                </div>
-                            </fieldset>
-                        </div>
-
-                        <button type="submit"
-                            className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add
-                            to bag</button>
-                    </form>
-                </div>
-
-                <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-                    <div>
-                        <h3 className="sr-only">Description</h3>
-
-                        <div className="space-y-6">
-                            <p className="text-base text-gray-900">{ data.fields["Description"] }</p>
-                        </div>
+                  <fieldset className="mt-4">
+                    <legend className="sr-only">Choose a size</legend>
+                    <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                      {sizes.map((item) => {
+                        return (
+                          <label className="group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6 cursor-pointer bg-white text-gray-900 shadow-sm">
+                            <input
+                              type="radio"
+                              name="size-choice"
+                              value={item}
+                              className="sr-only"
+                              aria-labelledby="size-choice-1-label"
+                            />
+                            <span id="size-choice-<% i %>-label">{item}</span>
+                            <span
+                              className="pointer-events-none absolute -inset-px rounded-md"
+                              aria-hidden="true"
+                            ></span>
+                          </label>
+                        );
+                      })}
                     </div>
+                  </fieldset>
                 </div>
+
+                {sizes.length ? (
+                  <button
+                    type="submit"
+                    className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Add to bag
+                  </button>
+                ) : (
+                  <button
+                    type="submit"
+                    className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-yellow-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Sold Out
+                  </button>
+                )}
+              </form>
             </div>
+
+            <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+              <div>
+                <h3 className="sr-only">Description</h3>
+
+                <div className="space-y-6">
+                  <p className="text-base text-gray-900">
+                    {data.fields["Description"]}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
